63d4eecacec56880e925296a0da1c05dba7e0f92
Melody <melody@pallet.town>
2018-12-05 02:41:40 -0500

    type effectiveness

--- include/battle_main.h
+++ include/battle_main.h
@@ -70,7 +70,7 @@ void RunBattleScriptCommands_PopCallbacksStack(void);
 void RunBattleScriptCommands(void);
 bool8 TryRunFromBattle(u8 battlerId);
 
-extern const u8 gTypeEffectiveness[336];
+extern const u8 gTypeEffectiveness[372];
 extern const u8 gTypeNames[][TYPE_NAME_LENGTH + 1];
 extern const struct TrainerMoney gTrainerMoneyTable[];
 extern const u8 gAbilityNames[][ABILITY_NAME_LENGTH + 1];
